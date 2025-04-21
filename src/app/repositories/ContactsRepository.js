e21210cd801d7d7c5c242c00a8931921095be2a8
Wigor Skorek <wigorskorek@gmail.com>
2025-04-21 13:24:05 -0300

    error handler added

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -13,12 +13,22 @@ class ContactRepository {
   }
 
   async findById(id) {
-    const row = await db.query(`SELECT * FROM contacts WHERE id = $1`, [id])
+    const row = await db.query(`
+      SELECT contacts.*, categories.name AS category_name
+      FROM contacts
+      LEFT JOIN categories ON categories.id = contacts.category_id
+      WHERE contacts.id = $1`
+
+      , [id])
     return row;
   }
 
   async findByEmail(email) {
-    const [row] = await db.query(`SELECT * FROM contacts WHERE email = $1`, [email]);
+    const [row] = await db.query(`
+      SELECT *
+      FROM contacts
+      WHERE email = $1`
+      , [email]);
     return row;
   }
 
